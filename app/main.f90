4fe7f726a8512cbfa1d9b320e2bd78247c876e8e
manuelimal02 <manuelimal02@gmail.com>
2024-02-21 10:02:18 -0600

    Lectura JSON

--- app/main.f90
+++ app/main.f90
@@ -1,14 +1,30 @@
 program PROYECTO_FASE_1
     use :: json_module
-
+    use cola_module
     implicit none
-    integer :: opcion_menu, opcion_menu_parametros
-    integer :: i, size, id, img_p, img_g 
+    !CLIENTE
+    type :: Cliente
+        character(len=:), allocatable :: id
+        character(len=:), allocatable :: nombre
+        character(len=:), allocatable :: img_p
+        character(len=:), allocatable :: img_g
+    end type Cliente
+    type(Cliente) :: mi_cliente
+    !LECTURA JSON
+    type(json_file) :: json
+    type(json_core) :: jsonc
+    type(json_value), pointer :: listPointer, animalPointer, attributePointer
     logical :: found
-    type(json_file) :: json   ! Se declara una variable del tipo json_file
-    type(json_value), pointer :: listPointer, personPointer, attributePointer  ! Se declaran punteros a variables del tipo json_value
-    type(json_core) :: jsonc  ! Se declara una variable del tipo json_core para acceder a las funciones básicas de JSON
-    character(:), allocatable :: nombre  ! Se declara una cadena de caracteres que se asignará dinámicamente
+    integer :: size, i
+    real(8) :: random_value
+    integer :: it, n, random_integer
+    character(:), allocatable :: id, nombre, img_peque, img_grande
+
+    !VARIABLES PARA EL USO DEL PROGRAMA
+    integer :: opcion_menu, opcion_menu_parametros
+
+    !COLA DE CLIENTES
+    type(cola) :: cola_recepcion
 
     do
         call Mostrar_Menu()
@@ -90,11 +106,57 @@ program PROYECTO_FASE_1
     subroutine Carga_Masiva_Clientes()
         print *, "---------------------------------------"
         print *, "-- Carga Masiva Cliente --"
+        call json%initialize()
+        call json%load(filename='config.json')
+        call json%info('',n_children=size)
+        call json%get_core(jsonc)
+        call json%get('', listPointer, found)
+    
+        do i = 1, size
+            call jsonc%get_child(listPointer, i, animalPointer, found)
+
+            call jsonc%get_child(animalPointer, 'id', attributePointer, found)
+            call jsonc%get(attributePointer, id)
+    
+            call jsonc%get_child(animalPointer, 'nombre', attributePointer, found)
+            call jsonc%get(attributePointer, nombre)
+    
+            call jsonc%get_child(animalPointer, 'img_p', attributePointer, found) 
+            call jsonc%get(attributePointer, img_peque)
+
+            call jsonc%get_child(animalPointer, 'img_g', attributePointer, found) 
+            call jsonc%get(attributePointer, img_grande)
+
+            mi_cliente%id = id
+            mi_cliente%nombre = nombre
+            mi_cliente%img_p = img_peque
+            mi_cliente%img_g = img_grande
+
+            print *, "----"
+            print *, 'ID: ', mi_cliente%id
+            print *, 'Nombre: ', mi_cliente%nombre
+            print *, 'img_p: ', mi_cliente%img_p
+            print *, 'img_g: ', mi_cliente%img_g
+
+            call cola_recepcion%append(1)
+
+        end do
+        call json%destroy()
+        call cola_recepcion%print()
     end subroutine
 
     subroutine Cantidad_Ventanillas()
         print *, "---------------------------------------"
         print *, "-- Cantidad Ventanillas --"
+        call random_seed()
+
+
+    ! Generar y mostrar los números aleatorios enteros
+    do i = 1, 1
+        call random_number(random_value)
+        random_integer = mod(int(random_value * 5.0 + 0.5), 5)  ! Escalar y redondear
+        print *, 'Numero aleatorio ', i, ': ', random_integer
+    end do
     end subroutine
 
 end program PROYECTO_FASE_1
--- src/EDD_PROYECTO1.f90
+++ src/EDD_PROYECTO1.f90
@@ -1,10 +1,72 @@
-module EDD_PROYECTO1
-  implicit none
-  private
+module cola_module
+    implicit none
+    private
 
-  public :: say_hello
+    type, public :: node
+        private
+        integer :: value
+        type(node), pointer :: next     
+    end type node
+
+    type, public :: cola
+        private
+        type(node), pointer :: head => null()
+        type(node), pointer :: tail => null()
+    contains
+        procedure :: append
+        procedure :: delete
+        procedure :: print
+    end type cola
+
 contains
-  subroutine say_hello
-    print *, "Hello, EDD_PROYECTO1!"
-  end subroutine say_hello
-end module EDD_PROYECTO1
+    subroutine append(this, value)
+        class(cola), intent(inout) :: this
+        integer, intent(in) :: value
+
+        type(node), pointer :: temp
+        allocate(temp)
+        temp%value = value
+        temp%next => null()
+
+        if (.not. associated(this%head)) then
+            this%head => temp
+            this%tail => temp
+        else
+            this%tail%next => temp
+            this%tail => temp
+        end if
+
+        print *, 'Append ', value
+    end subroutine append
+
+    subroutine delete(this)
+        class(cola), intent(inout) :: this
+        type(node), pointer :: temp
+
+        if (.not. associated(this%head)) then
+            print *, 'Cola esta vacia'
+            return
+        end if
+
+        print *, 'Delete ', this%head%value
+        temp => this%head
+        this%head => this%head%next
+        deallocate(temp)
+    end subroutine delete
+
+    subroutine print(this)
+        class(cola), intent(in) :: this
+        type(node), pointer :: current
+
+        current => this%head
+
+        print *, '//-----------------//'
+        print *, 'La cola es:'
+        print *, '//-----------------//'
+
+        do while (associated(current))
+            print *, current%value
+            current => current%next
+        end do 
+    end subroutine print
+end module cola_module
