96c3f2130b7662b23c985f298c2bcba813c8cfe2
manuelimal02 <manuelimal02@gmail.com>
2024-04-27 14:38:34 -0600

    Fase 3

---
+++ PROYECTO_FASE3/src/PROYECTO_FASE3.f90
@@ -0,0 +1,10 @@
+module PROYECTO_FASE3
+  implicit none
+  private
+
+  public :: say_hello
+contains
+  subroutine say_hello
+    print *, "Hello, PROYECTO_FASE3!"
+  end subroutine say_hello
+end module PROYECTO_FASE3
